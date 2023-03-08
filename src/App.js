5035c329b971495d5aa076c9ebc297d1c3bed59f
Mohammad Mostafa Hareb <61649616+mmostafahareb@users.noreply.github.com>
2023-03-08 17:54:24 +0000

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { AiFillHeart } from 'react-icons/ai';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(1);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  }
+
+  const handleDecrement = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    } else {
+      setCount(count);
+      console.error('Counter cannot be less than 1!');
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>React Counter and Icons Demo</h1>
+      <div className="counter-container">
+        <button className="counter-button" onClick={handleDecrement}>-</button>
+        <p className="counter-text">{count}</p>
+        <button className="counter-button" onClick={handleIncrement}>+</button>
+      </div>
+      <div className="heart-container">
+        <AiFillHeart className="heart-icon" size={count} color="red" />
+        <p className="heart-text">The heart size is {count}px!</p>
+      </div>
+      {count < 0 && (
+        <p className="error-text">Error: Counter cannot be negative!</p>
+      )}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,47 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders counter and heart icon', () => {
+  const { getByText, getByTestId } = render(<App />);
+  
+  // Check if the counter and heart icon are rendered
+  const counter = getByText(/count:/i);
+  expect(counter).toBeInTheDocument();
+  const heartIcon = getByTestId('heart-icon');
+  expect(heartIcon).toBeInTheDocument();
+});
+
+test('increments and decrements counter', () => {
+  const { getByText, getByTestId } = render(<App />);
+
+  // Click the increment button and check if count increases
+  const incrementButton = getByText('+');
+  fireEvent.click(incrementButton);
+  const counter = getByText(/count: 1/i);
+  expect(counter).toBeInTheDocument();
+  
+  // Click the decrement button and check if count decreases
+  const decrementButton = getByText('-');
+  fireEvent.click(decrementButton);
+  const updatedCounter = getByText(/count: 0/i);
+  expect(updatedCounter).toBeInTheDocument();
+});
+
+test('changes heart icon size with counter', () => {
+  const { getByTestId } = render(<App />);
+
+  // Check if the heart icon size is initially 0
+  const heartIcon = getByTestId('heart-icon');
+  expect(heartIcon).toHaveStyle('font-size: 0px');
+
+  // Increment count and check if the heart icon size increases
+  const incrementButton = getByText('+');
+  fireEvent.click(incrementButton);
+  expect(heartIcon).toHaveStyle('font-size: 1px');
+
+  // Decrement count and check if the heart icon size decreases
+  const decrementButton = getByText('-');
+  fireEvent.click(decrementButton);
+  expect(heartIcon).toHaveStyle('font-size: 0px');
 });
